681ed018ce02169c9a8c50191b5d8cd6879fd825
Estevao Souza <estevaorssouza@gmail.com>
2023-03-14 02:29:29 +0000

    Add Message error in Component Login, and ajust Context and Provider.

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -4,6 +4,7 @@ import { IUserMap, LoginUser, Response } from "../interface/LoginUser";
 type UserContextType = {
   User: IUserMap | null;
   Token: string | null;
+  Message: string | null;
 
   LoginAccount(payload: LoginUser): Promise<boolean>;
   ValidateToken(token: string): Promise<IUserMap | null>;
